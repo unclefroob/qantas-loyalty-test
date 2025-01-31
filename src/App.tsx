427d3242c197016452bdc96e7146c8af557554c1
unclefroob <ryan@ardorio.co>
2025-01-31 16:32:18 +1100

    add header component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import qantasLogo from "./assets/qantas-logo.png";
 import data from "./assets/data.json";
 import "./App.css";
 import ListItem from "./components/ListItem";
+import Header from "./components/Header";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <a href="https://react.dev" target="_blank">
         <img src={qantasLogo} alt="Qantas Logo" />
       </a>
+      <Header numberOfHotels={data.results.length} />
       {data.results.map((item) => (
         <ListItem key={item.id} item={item} />
       ))}
---
+++ src/components/Header.tsx
@@ -0,0 +1,30 @@
+import { Box, styled, Typography } from "@mui/material";
+
+interface IHeaderProps {
+  numberOfHotels: number;
+}
+
+const HeaderBox = styled(Box)({
+  display: "flex",
+  justifyContent: "space-between",
+  padding: "0.4rem",
+});
+
+const SortByContainer = styled(Box)({
+  display: "flex",
+});
+
+const Header = (props: IHeaderProps) => {
+  const { numberOfHotels } = props;
+  return (
+    <HeaderBox>
+      <Typography>{numberOfHotels} hotels in Sydney</Typography>
+      <SortByContainer>
+        <Typography sx={{ marginRight: "0.2rem" }}>Sort by</Typography>
+        <select></select>
+      </SortByContainer>
+    </HeaderBox>
+  );
+};
+
+export default Header;
