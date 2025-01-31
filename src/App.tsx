509fcc60abc6966deb7a6411af3869adbcff409e
unclefroob <ryan@ardorio.co>
2025-01-31 18:38:27 +1100

    redirect logo back to homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,24 @@
 import qantasLogo from "./assets/qantas-logo.png";
 import data from "./assets/data.json";
-import "./App.css";
 import ListItem from "./components/ListItem";
 import Header from "./components/Header";
+import { Box, styled } from "@mui/material";
+
+const AppBox = styled(Box)({
+  width: "100%",
+});
 
 const App = () => {
   return (
-    <>
-      <a href="https://react.dev" target="_blank">
+    <AppBox>
+      <a href="https://localhost:5173" target="_blank">
         <img src={qantasLogo} alt="Qantas Logo" />
       </a>
       <Header numberOfHotels={data.results.length} />
       {data.results.map((item) => (
         <ListItem key={item.id} item={item} />
       ))}
-    </>
+    </AppBox>
   );
 };
 
